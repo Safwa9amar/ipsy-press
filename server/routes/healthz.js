a8f5524eecbd5785ee81e1bfd8fe7695d77f36c3
Safwa9amar <hassanih97@gmail.com>
2023-07-02 12:24:27 +0200

    up

--- server/routes/healthz.js
+++ server/routes/healthz.js
@@ -4,10 +4,12 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res, next) {
   const url = req.protocol + "://" + req.get("host");
-  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  const port = req.socket.remotePort || process.env.PORT 
   console.log("====================================");
   console.log("server ip is ---> " + ip);
   console.log("server host  ---> " + url);
+  console.log("server port  ---> " + port);
   console.log("====================================");
 
   res.sendStatus(200);
